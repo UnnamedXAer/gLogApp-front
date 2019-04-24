c987c4b2e83d9529ca84f762f763b6ba572c6051
unnamed <>
2019-04-24 15:11:26 +0200

    login route changed to use passport (temporary)

--- src/containers/Users/Login/Login.js
+++ src/containers/Users/Login/Login.js
@@ -36,7 +36,7 @@ class Login extends React.Component {
         }
         else {
             this.setState({showSpinner: true});
-            axios.post('/user/login', {login: login, password: password})
+            axios.post('/api/login', {login: login, password: password})
                 .then(res => {
                     if (res.data.errors) {
                         this.setState({validationErrors: res.data.errors, showSpinner: false});
