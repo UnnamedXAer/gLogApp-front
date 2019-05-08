057e83eabb71fb1faba22e38881b5a38cbd37831
unnamed <>
2019-05-08 16:23:33 +0200

    new component StartPrompt for training screen

---
+++ src/components/Training/StartPrompt/StartPrompt.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import classes from './StartPrompt.module.css';
+import Modal from '../../UI/Modal/Modal';
+
+const startPrompt = props => {
+
+    let trainings = null; 
+    if (props.trainings) {
+        trainings = <ol>
+                {props.trainings.map(training => <li key={training.id} onClick={props.trainingSelected}>{training.startTime}</li>)}
+            </ol>
+    }
+    return (
+        <Modal show={props.show} >
+            <div className={classes.StartPrompt}>
+                {trainings? <div className={classes.SavedTrainings}>
+                        <h4>The following trainings are not completed select one of them or start new.</h4> 
+                        {trainings.length > 1 ? <p>You should go to <Link to="/training-history">Training History</Link> and fix uncompleted trainings.</p> : null}
+                        {trainings}
+                    </div>: null}
+                <div className={classes.Buttons}>
+                    <button onClick={props.confirm}>Start New Training</button>
+                </div>
+            </div>
+        </Modal>
+    );
+}
+
+export default startPrompt;
